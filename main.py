07adf0fda6812eaf56a427671eb9cd8ef28f00e8
Ayush <6427045+kumaagx@users.noreply.github.com>
2022-06-01 14:21:34 +0000

    Revisit this project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 exec(open("Code/Collect_NYT.py").read())
 exec(open("Code/Collect_BuzzFeed.py").read())
-exec(open("Code/Collect_TOI.py").read())
+# exec(open("Code/Collect_TOI.py").read())
 exec(open("Code/Collect_BBC.py").read())
